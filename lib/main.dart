dd6c946f78637acb6fab4605e2cd2367c8945638
Felix Lehmann <lehmannfelix77gmail.com>
2022-04-11 14:17:08 +0200

    Create and initialize the CameraController

--- lib/main.dart
+++ lib/main.dart
@@ -33,54 +33,106 @@ class _MainAppState extends State<MainApp> {
     return MaterialApp(
       home: Scaffold(
         body: FutureBuilder(
-            future: _initCameras(),
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-              List<Widget> children;
-              if (snapshot.hasData) {
-                children = <Widget>[
-                  const Icon(
-                    Icons.check_circle_outline,
-                    color: Colors.green,
-                    size: 60,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text('Result: ${snapshot.data}'),
-                  )
-                ];
-              } else if (snapshot.hasError) {
-                children = <Widget>[
-                  const Icon(
-                    Icons.error_outline,
-                    color: Colors.red,
-                    size: 60,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text('Error: ${snapshot.error}'),
-                  )
-                ];
-              } else {
-                children = const <Widget>[
-                  SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: CircularProgressIndicator(),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 16),
-                    child: Text('Awaiting result...'),
-                  )
-                ];
-              }
-              return Center(
+          future: _initCameras(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            Widget _widget;
+            if (snapshot.hasData) {
+              CameraDescription _cameraDescription = snapshot.data;
+              _widget = TakePictureScreen(camera: _cameraDescription)
+                  // const Icon(
+                  //   Icons.check_circle_outline,
+                  //   color: Colors.green,
+                  //   size: 60,
+                  // ),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(top: 16),
+                  //   child: Text('Result: ${snapshot.data}'),
+                  // )
+                  ;
+            } else if (snapshot.hasError) {
+              _widget = Center(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: children,
+                  children: [
+                    const Icon(
+                      Icons.error_outline,
+                      color: Colors.red,
+                      size: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Text('Error: ${snapshot.error}'),
+                    )
+                  ],
                 ),
               );
-            }),
+            } else {
+              _widget = Center(
+                child: Column(
+                  children: const [
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: CircularProgressIndicator(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 16),
+                      child: Text('Awaiting result...'),
+                    )
+                  ],
+                ),
+              );
+            }
+            return _widget;
+          },
+        ),
       ),
     );
+  }
+}
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next steps.
+    return Container();
   }
 }
